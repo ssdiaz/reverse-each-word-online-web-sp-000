bff1ad5eaa97e2dc72e55dff6433f89cd51a9bdb
Sam D <samidiaz3@gmail.com>
2021-01-31 21:44:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,4 @@
 def reverse_each_word (string)
-  array = string.split " "
-
-  new_array = []
-
-  array.each.collect do |word|
-    new_array << word.reverse
-  end
-     new_array.join(" ")
-
-
-
-array = string.split.collect {|word| word.reverse}
-
-array.join(" ")
-
-
-
-
+  array = string.split.collect {|word| word.reverse}
+  array.join(" ")
 end

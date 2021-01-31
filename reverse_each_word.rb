d7aa738296b5cbf7ca47ac3f803fa3afa67465b0
Sam D <samidiaz3@gmail.com>
2021-01-31 21:29:00 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word (string)
+
+#turn string into array for each word
+# reverse each word in array .reverese
+
+string.reverse
+array = string.reverse.split " "
+array. join(" ")
+end
+
+# string = "Hello there, and how are you?"

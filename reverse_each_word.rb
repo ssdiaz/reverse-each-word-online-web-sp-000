d7c13c01d61147f3e0da31ec9d53b9108ed4b37d
Sam D <samidiaz3@gmail.com>
2021-01-31 21:39:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,10 @@
 def reverse_each_word (string)
-# convert to array
-array = string.split " "
-new_array = []
-# use .each to reverse each element?
-array.each.map do |word|
-  new_array << word.reverse
-end
-   new_array.join(" ")
-end
+  array = string.split " "
 
+  new_array = []
 
-# string = "Hello there, and how are you?"
+  array.each.map do |word|
+    new_array << word.reverse
+  end
+     new_array.join(" ")
+end
